5e2ebfdbb021df7d8e83b240806bffbf3e085dc7
Wallis <wallis.sl@outlook.com>
2023-02-23 13:11:42 -0300

    teste estudo

--- M1/M1S02/M1S02EX8.js
+++ M1/M1S02/M1S02EX8.js
@@ -1,39 +1,53 @@
-//prompt ("Digite um número");
-
-let iniciarCompra = document.getElementById("enviar");
-iniciarCompra.onclick = clicou;
-
-var number = "";
-
-function numberUsuario(){
-  
-    do{
-    
-    var numeroSoma = "";
-    
-
-    number = prompt("Digite um número");
-    // numeroSoma = numeroSoma + number;
-
+const setPriceFromPrompt = (product) => {
+    let price = parseFloat(prompt(`Insira o preço do produto ${product}:`));
+    if (isNaN(price)) {
+      setPriceFromPrompt(product);
     }
-    while(number != 0) 
-
-}
-
-
-
-
-function clicou(){
-    numberUsuario();  
-    
-}
-
-console.log(number);
-
-
-/* const handleChangeNumero = (e) => {
-    usuario.numero = e.target.value;
-}
-
-iniciarCompra.addEventListener("change", handleChangeNumero); */
-
+  
+    return price;
+  };
+  
+  const setClientCashFromPrompt = (total) => {
+    let cash = parseFloat(
+      prompt(`Insira o valor em dinheiro que o cliente forneceu:`)
+    );
+    if (isNaN(cash)) {
+      setClientCashFromPrompt(total);
+    }
+    return cash;
+  };
+  
+  const inputPriceProduct = () => {
+    let product = 1;
+    let price = setPriceFromPrompt(product);
+    let total = 0;
+    let results = `### Lojas Tabajara ### \n`;
+    results += `========================= \n`;
+    while (price > 0) {
+      total += price;
+      results += `+ Produto ${product}: R$ ${price}\n`;
+      product += 1;
+      price = setPriceFromPrompt(product);
+    }
+  
+    alert("Final da compra");
+    let cash = setClientCashFromPrompt(total);
+    while (total > cash) {
+      cash = setClientCashFromPrompt(total);
+    }
+  
+    results += `========================= \n`;
+    results += `Total: R$ ${total}\n`;
+    results += `Dinheiro: R$ ${cash}\n`;
+    results += `Troco: R$ ${cash - total}\n`;
+    results += `========================= \n`;
+    alert(results);
+  };
+  
+  function padariaTechPlusInit() {
+    document.querySelector("#enviar").innerHTML = `
+          <button type="button" onclick="inputPriceProduct()">Caixa Registradora</button>
+          `;
+  }
+  
+  padariaTechPlusInit();
