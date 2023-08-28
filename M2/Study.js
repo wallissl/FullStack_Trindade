9c58354cd1eb4afc3880a5d6aa29e7405d804f80
Wallis <wallis.sl@outlook.com>
2023-08-28 17:14:31 -0300

    :Estudando

---
+++ M2/Study.js
@@ -0,0 +1,53 @@
+/* Aqui vamos iniciar os estudos em html - M2 S2A4 */
+
+// HTML 
+
+// Podemos utilizar os elementos get e querySelector para capturar elementos no html para o Js
+
+/*  
+<button id="meuBotao">Enviar</button>
+document.getElementById("meuBotao"); // Pegar um item
+document.querySelector(".") // Pegar um item
+document.querySelectorAll(".") // Pegar vários itens
+
+*/
+
+console.log('Hello World');
+
+/* 
+const button = document.getElementById("meuOutroButton");
+
+/* Uma forma de chamar: 
+button.addEventListener('click', event => {
+    console.log('Você clicou');}) 
+
+// Outra forma
+button.addEventListener('click', oi);
+
+function oi(){
+    console.log('Você clicou')
+}  */
+
+// Forms S2A4
+
+function enviarFormulario(event){
+    event.preventDefault(); // Evitar o comportamento padrão do forms
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var mensagem = document.getElementById('mensagem').value;
+
+    console.log('Nome',nome);
+    console.log('Email', email);
+    console.log('Mensagem',mensagem);
+
+    const usuario = {
+        nome: nome,
+        email: email,
+        mensagem: mensagem
+    }
+    
+}
+
+
+
+// S3A1 00:12
