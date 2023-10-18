3bfba26e7fe28ac4ae64019ee3d795d083440116
Wallis <wallis.sl@outlook.com>
2023-10-17 21:05:07 -0300

    :EX1 em andamento

--- M2/S02/S2EX1.js
+++ M2/S02/S2EX1.js
@@ -1,7 +1,6 @@
-function fazerAlgo(){
+/* function fazerAlgo(){
     console.log('ola')
 }
 
 const botao = document.getElementById("pri")
-botao.addEventListener('click', fazerAlgo);
-
+botao.addEventListener('click', fazerAlgo); */ // Função que é ativa quando clica no botão.
