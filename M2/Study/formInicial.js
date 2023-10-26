d8f6ebc7af32ef82430be0b6a0ff52294ee7bce4
Wallis <wallis.sl@outlook.com>
2023-10-26 17:32:54 -0300

    Study

---
+++ M2/Study/formInicial.js
@@ -0,0 +1,24 @@
+// Uma forma de resolver ws
+/* let enviar = document.getElementById("enviar");
+let email = document.getElementById('email');
+let senha = document.getElementById('senha');
+
+function clicou(event){
+    event.preventDefault()
+    console.log(email.value);
+    console.log(senha.value)
+}
+enviar.addEventListener("click",clicou);  */
+
+
+// Forma de realizar com função direto no html
+/* function processarLogin(event){
+    event.preventDefault();
+    console.log(email.value);
+    console.log(senha.value)
+} */
+
+
+// Forma caso queira chamar a função direto pelo JavaScript
+/* const formLogin = document.getElementById("formLogin");
+formLogin.addEventListener('submit', processarLogin) */
